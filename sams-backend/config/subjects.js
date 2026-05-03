e2061ef3b9181c3571be18d0d11eea9d2db7b8fe
Reetik Kumar <reetikkumar736997@gmail.com>
2026-05-03 17:30:48 +0530

    Expand class subject options

--- sams-backend/config/subjects.js
+++ sams-backend/config/subjects.js
@@ -1,16 +1,16 @@
 const SUBJECTS_BY_CLASS = {
-  "1": ["English", "Hindi", "Mathematics", "EVS", "Drawing"],
-  "2": ["English", "Hindi", "Mathematics", "EVS", "General Knowledge"],
-  "3": ["English", "Hindi", "Mathematics", "EVS", "Computer"],
-  "4": ["English", "Hindi", "Mathematics", "Science", "Social Studies", "Computer"],
-  "5": ["English", "Hindi", "Mathematics", "Science", "Social Studies", "Computer"],
-  "6": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "Computer"],
-  "7": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "Computer"],
-  "8": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "Computer"],
-  "9": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Information Technology"],
-  "10": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Information Technology"],
-  "11": ["English", "Physics", "Chemistry", "Biology", "Mathematics", "Computer Science", "Accountancy", "Business Studies", "Economics", "History", "Political Science"],
-  "12": ["English", "Physics", "Chemistry", "Biology", "Mathematics", "Computer Science", "Accountancy", "Business Studies", "Economics", "History", "Political Science"]
+  "1": ["English", "Hindi", "Mathematics", "EVS", "Drawing", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "2": ["English", "Hindi", "Mathematics", "EVS", "Drawing", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "3": ["English", "Hindi", "Mathematics", "EVS", "Science", "Social Studies", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "4": ["English", "Hindi", "Mathematics", "EVS", "Science", "Social Studies", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "5": ["English", "Hindi", "Mathematics", "EVS", "Science", "Social Studies", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "6": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "7": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "8": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "General Knowledge", "Computer", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "9": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "Information Technology", "Computer Applications", "General Knowledge", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "10": ["English", "Hindi", "Mathematics", "Science", "Social Science", "Sanskrit", "Information Technology", "Computer Applications", "General Knowledge", "Moral Science", "Art And Craft", "Music", "Physical Education"],
+  "11": ["English", "Physics", "Chemistry", "Biology", "Mathematics", "Computer Science", "Information Practices", "Accountancy", "Business Studies", "Economics", "History", "Political Science", "Geography", "Psychology", "Sociology", "Physical Education", "Fine Arts"],
+  "12": ["English", "Physics", "Chemistry", "Biology", "Mathematics", "Computer Science", "Information Practices", "Accountancy", "Business Studies", "Economics", "History", "Political Science", "Geography", "Psychology", "Sociology", "Physical Education", "Fine Arts"]
 };
 
 module.exports = { SUBJECTS_BY_CLASS };
